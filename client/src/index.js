cfe4f68265833a3fa3a3649e2da0b64bdbafec6f
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-16 08:15:07 -0700

    updating client

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import "semantic-ui-css/semantic.min.css"
+import './App.css'
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
